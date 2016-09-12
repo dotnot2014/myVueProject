e09f43269ac9b48a5767c221485f3dd7054028ad
吴印良品 <wwj@Weijuns-MacBook-Air.local>
2016-09-12 11:54:56 +0800

    webpack配置文件添加注释

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,15 +3,19 @@ var config = require('../config')
 var utils = require('./utils')
 var projectRoot = path.resolve(__dirname, '../')
 
+
 module.exports = {
+  // 入口文件，是模块构建的起点，同时每一个入口文件对应最后生成的一个 chunk。
   entry: {
     app: './src/main.js'
   },
+  // 生成文件，是模块构建的终点，包括输出文件与输出路径。
   output: {
     path: config.build.assetsRoot,
     publicPath: process.env.NODE_ENV === 'production' ? config.build.assetsPublicPath : config.dev.assetsPublicPath,
     filename: '[name].js'
   },
+  // 文件路径指向(可加快打包过程)。
   resolve: {
     extensions: ['', '.js', '.vue'],
     fallback: [path.join(__dirname, '../node_modules')],
@@ -24,6 +28,7 @@ module.exports = {
   resolveLoader: {
     fallback: [path.join(__dirname, '../node_modules')]
   },
+  // 这里配置了处理各模块的 loader ，包括 css 预处理 loader ，es6 编译 loader，图片处理 loader
   module: {
     loaders: [
       {
